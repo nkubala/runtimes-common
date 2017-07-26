266a1880f0e9fd4c5ff011b5163091b32778a917
sharifelgamal <selgamal@google.com>
2017-07-26 00:29:32 -0700

    github api maybe?

--- gpg-demo/gpg-demo.sh
+++ gpg-demo/gpg-demo.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+sha=$(curl -X GET -H 'Accept: application/vnd.github.VERSION.sha' https://api.github.com/repos/nkubala/runtimes-common/commits/HEAD)
+resp=$(curl -X GET -H 'Accept: application/vnd.github.cryptographer-preview' https://api.github.com/repos/nkubala/runtimes-common/commits/$sha)
 
-git verify-commit HEAD
+echo $resp
