5a200a0db0749cdbfb54e52f412b9fa89582cfe8
sharifelgamal <selgamal@google.com>
2017-07-26 10:36:53 -0700

    testing git commands

--- gpg-demo/gpg-demo.sh
+++ gpg-demo/gpg-demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sha=$(curl -X GET -H 'Accept: application/vnd.github.VERSION.sha' https://api.github.com/repos/nkubala/runtimes-common/commits/HEAD)
-resp=$(curl -X GET -H 'Accept: application/vnd.github.cryptographer-preview' https://api.github.com/repos/nkubala/runtimes-common/commits/$sha)
 
-echo $resp
+cd github/runtimes-common
+
+git verify-commit HEAD
