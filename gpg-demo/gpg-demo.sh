ba4577c4ccd83a00bab313b6fa59d2808f004b4e
sharifelgamal <selgamal@google.com>
2017-07-27 11:50:40 -0700

    actually try checking the signature now

--- gpg-demo/gpg-demo.sh
+++ gpg-demo/gpg-demo.sh
@@ -5,12 +5,20 @@ set -ex
 # Pull down authorized public keys and add them as trusted
 users=(${AUTHORIZED_USERS})
 
-ls $KOKORO_GFILE_DIR
+# prefixes are of the form "pub   2048D/"
+key_prefix_length=12
+key_length=16
 
 for user in ${users[@]}
 do
-  echo $user
-  cat $KOKORO_GFILE_DIR/$user.asc
+  # Import the public key
+  gpg --import $KOKORO_GFILE_DIR/$user.asc
+
+  # Add it to the set of trusted keys
+  full_key_line=$(gpg --list-keys --keyid-format LONG $user | grep pub)
+  key=${full_key_line:$key_prefix_length:$key_length}
+
+  echo "trusted-key 0x${key}" >> ~/.gnupg/gpg.conf
 done
 
 cd github/runtimes-common
