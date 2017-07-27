bfb82a81c2e1b2630740e8da05ad016686f0e79c
sharifelgamal <selgamal@google.com>
2017-07-27 11:22:03 -0700

    changing where I look for keys

--- gpg-demo/gpg-demo.sh
+++ gpg-demo/gpg-demo.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+set -ex
+
 # Pull down authorized public keys and add them as trusted
 users=(${AUTHORIZED_USERS})
+
+ls $KOKORO_GFILE_DIR
 
 for user in ${users[@]}
 do
   echo $user
+  cat $KOKORO_GFILE_DIR/$user.asc
 done
-
-ls $KOKORO_GFILE_DIR
-touch $KOKORO_GFILE_DIR/copy.bara.sky
 
 cd github/runtimes-common
 
