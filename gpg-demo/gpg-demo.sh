53cf67cbe518e9e8c997bd52d7687b92762d7132
sharifelgamal <selgamal@google.com>
2017-07-27 10:43:32 -0700

    testing out verify script

--- gpg-demo/gpg-demo.sh
+++ gpg-demo/gpg-demo.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Pull down authorized public keys and add them as trusted
+users=(${AUTHORIZED_USERS})
+
+for user in ${users[@]}
+do
+  echo $user
+done
+
+ls $KOKORO_GFILE_DIR
+touch $KOKORO_GFILE_DIR/copy.bara.sky
+
 cd github/runtimes-common
 
 git verify-commit HEAD
